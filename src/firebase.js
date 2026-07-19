0feb525f1297c7e1a3d95d87a07bc33c3218fff5
markmontanogeniy-cell <markmontanogeniy@gmail.com>
2026-07-19 03:07:04 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getDatabase, ref, onValue, set, get } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyDMsIwIiY7zdWy5zKjFmxqmzaBJT7ePyHU",
   authDomain: "mirovoe-gospodstvo-91462.firebaseapp.com",
-  databaseURL: "https://mirovoe-gospodstvo-91462-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://mirovoe-gospodstvo-91462-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "mirovoe-gospodstvo-91462",
   storageBucket: "mirovoe-gospodstvo-91462.firebasestorage.app",
   messagingSenderId: "349228047052",
